e538e9f3732c91fce0432bfc7d664c46fda89df2
Görkem <gorkem_yelken@hotmail.com>
2024-11-17 18:45:31 +0300

    Updated

--- src/main/java/com/aranmakina/backend/model/CategoryType.java
+++ src/main/java/com/aranmakina/backend/model/CategoryType.java
@@ -1,17 +1,18 @@
 package com.aranmakina.backend.model;
 
 public enum CategoryType {
-    SABIT_ASKILI_ZINCIRLI_VINC,
-    MANUEL_SARYOLU_ZINCIRLI_VINC,
-    MONORAY_ZINCIRLI_VINC,
-    SABIT_HALATLI_VINC,
-    MANUEL_SARYOLU_HALATLI_VINC,
-    MONORAY_HALATLI_VINC,
-    IRGAT_VINC,
-    MINI_VINC,
-    TRANSPALET,
-    MEKANIK_CARASKAL,
-    SARYO,
-    MAKARA,
-    KUMANDA
+    sabitAskiliZincirliVinc,
+    manuelSaryoluZincirliVinc,
+    monorayZincirliVinc,
+    sabitHalatliVinc,
+    manuelSaryoluHalatliVinc,
+    monorayHalatliVinc,
+    irgatVinc,
+    miniVinc,
+    transpalet,
+    mekanikCaraskal,
+    saryo,
+    makara,
+    kumanda
 }
+
